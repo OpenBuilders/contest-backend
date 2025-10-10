1d06dbe455730cd54e9101870430dfc53e51debb
Erfan Mola <hi@erfanxd.ir>
2025-10-10 22:24:07 +0330

    feat: Verify Transaction via BOC

--- api/routes/contest-submit.ts
+++ api/routes/contest-submit.ts
@@ -1,3 +1,4 @@
+import { sleep } from "bun";
 import type { Handler } from "elysia";
 import type { Insertable } from "kysely";
 import z from "zod";
@@ -5,6 +6,7 @@ import type { JWTInjections, PoolInjections } from "../../api";
 import { limits } from "../../information/limits";
 import type { DBSchema } from "../../schema";
 import { events } from "../../utils/events";
+import { verifyTransaction } from "../../utils/ton";
 
 const validator = z.preprocess(
 	(data: any) => {
@@ -15,6 +17,7 @@ const validator = z.preprocess(
 			.string()
 			.min(limits.form.participate.description.minLength)
 			.max(limits.form.participate.description.maxLength + 256),
+		wallet: z.string().optional(),
 		boc: z.string().optional(),
 	}),
 );
@@ -25,13 +28,30 @@ export const routePOSTContestSubmit: Handler = async (ctx) => {
 	const schema = validator.safeParse(ctx.body);
 
 	if (schema.success) {
-		const contest: Pick<DBSchema["contests"], "id"> | undefined = await db
-			.selectFrom("contests")
-			.select(["id"])
-			.where("slug", "=", ctx.params.slug)
-			.executeTakeFirst();
+		const contest: Pick<DBSchema["contests"], "id" | "fee"> | undefined =
+			await db
+				.selectFrom("contests")
+				.select(["id", "fee"])
+				.where("slug", "=", ctx.params.slug)
+				.executeTakeFirst();
 
 		if (contest) {
+			let payment_valid = true;
+
+			if (contest.fee > 0) {
+				payment_valid = false;
+				let tries = 0;
+
+				do {
+					await sleep(5_000);
+					payment_valid = await verifyTransaction(
+						schema.data.boc ?? "",
+						schema.data.wallet ?? "",
+					);
+					tries++;
+				} while (!payment_valid && tries <= 10);
+			}
+
 			const entry = await db
 				.selectFrom("submissions")
 				.select(["id"])
@@ -39,7 +59,7 @@ export const routePOSTContestSubmit: Handler = async (ctx) => {
 				.where("user_id", "=", user_id)
 				.executeTakeFirst();
 
-			if (!entry) {
+			if (!entry && payment_valid) {
 				const { data } = schema;
 
 				const value: Insertable<DBSchema["submissions"]> = {
@@ -50,8 +70,6 @@ export const routePOSTContestSubmit: Handler = async (ctx) => {
 					}),
 					user_id: user_id as any,
 				};
-
-				// TODO: validate payment
 
 				await db.insertInto("submissions").values(value).execute();
 
--- utils/ton.ts
+++ utils/ton.ts
@@ -1,7 +1,53 @@
-import { TonClient } from "@ton/ton";
+import { Address, beginCell, Cell, storeMessage, TonClient } from "@ton/ton";
 import { env } from "./env";
 
 export const tonClient = new TonClient({
 	endpoint: env.TON_CLIENT_ENDPOINT_URL,
 	apiKey: env.TON_CLIENT_API_KEY,
 });
+
+export const verifyTransaction = async (
+	bocBase64: string,
+	wallet_address: string,
+) => {
+	const cell = Cell.fromBase64(bocBase64);
+	const address = Address.parse(wallet_address);
+	const messageHash = cell.hash().toString("hex");
+
+	const transactions = await tonClient.getTransactions(address, {
+		limit: 10,
+		archival: true,
+	});
+
+	const txs = transactions.map((tx) => {
+		const hashes = [];
+		hashes.push(tx.hash().toString("hex"));
+
+		if (tx.inMessage) {
+			const cell = beginCell().store(storeMessage(tx.inMessage)).endCell();
+			hashes.push(cell.hash().toString("hex"));
+		}
+
+		for (const msg of tx.outMessages.values()) {
+			const cell = beginCell().store(storeMessage(msg)).endCell();
+			hashes.push(cell.hash().toString("hex"));
+		}
+
+		return {
+			tx,
+			hashes,
+		};
+	});
+
+	const tx = txs.find((i) => i.hashes.includes(messageHash));
+
+	if (tx) {
+		if (tx.tx.description.type === "generic") {
+			if (tx.tx.description.computePhase.type === "vm") {
+				return tx.tx.description.computePhase.success ?? false;
+			}
+		}
+	}
+
+	return false;
+};
