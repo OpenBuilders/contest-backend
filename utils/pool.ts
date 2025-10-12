ffa81c05344ddee58a8e82df05ed0bdf8dd7323e
Erfan Mola <hi@erfanxd.ir>
2025-10-12 19:52:09 +0330

    feat: Add connection retry on close for pgsql

--- utils/pool.ts
+++ utils/pool.ts
@@ -1,19 +1,10 @@
 import genereicPool from "generic-pool";
-import { Pool } from "pg";
 import { createClientPool } from "redis";
 import { env } from "./env";
+import { pg } from "./pg";
 
 export const pools = {
-	pg: new Pool({
-		host: env.PGSQL_HOST,
-		user: env.PGSQL_USER,
-		password: env.PGSQL_PASS,
-		port: env.PGSQL_PORT,
-		database: env.PGSQL_NAME,
-		idleTimeoutMillis: 60 * 60_000,
-		max: env.POOL_SIZE_PGSQL,
-		min: 1,
-	}),
+	pg: pg,
 	redis: createClientPool(
 		{
 			socket: {
