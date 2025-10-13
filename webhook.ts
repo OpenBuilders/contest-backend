0ab240e6221cc13c3198a62b3ec42e14c0a96a38
Erfan Mola <hi@erfanxd.ir>
2025-10-13 23:35:15 +0330

    revert: Logout method

--- webhook.ts
+++ webhook.ts
@@ -1,12 +1,7 @@
-import { logOut, setWebhook } from "nyx-bot-client";
+import { setWebhook } from "nyx-bot-client";
 import { env } from "./utils/env";
 
 export const initializeWebhook = async () => {
-	await logOut({
-		bot_api_server: env.BOT_API_SERVER,
-		bot_token: env.BOT_TOKEN,
-	});
-
 	return setWebhook({
 		url: env.WEBHOOK_URL,
 		bot_api_server: env.BOT_API_SERVER,
