88e8c45dd1703de241f877b2de070f8c43bea126
Erfan Mola <hi@erfanxd.ir>
2025-10-13 23:26:52 +0330

    dev: Debug double update issue

--- webhook.ts
+++ webhook.ts
@@ -1,7 +1,12 @@
-import { setWebhook } from "nyx-bot-client";
+import { logOut, setWebhook } from "nyx-bot-client";
 import { env } from "./utils/env";
 
-export const initializeWebhook = () => {
+export const initializeWebhook = async () => {
+	await logOut({
+		bot_api_server: env.BOT_API_SERVER,
+		bot_token: env.BOT_TOKEN,
+	});
+
 	return setWebhook({
 		url: env.WEBHOOK_URL,
 		bot_api_server: env.BOT_API_SERVER,
