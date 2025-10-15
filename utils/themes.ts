8cc12a7375658e2b7153eac6bb4411e3f33cd86d
Erfan Mola <hi@erfanxd.ir>
2025-10-15 23:08:41 +0330

    feat: Add themes and icons

--- utils/themes.ts
+++ utils/themes.ts
@@ -10,16 +10,16 @@ export type ContestThemeBackdrop = {
 };
 
 export const ContestThemeBackdrops: ContestThemeBackdrop[] = [
-	// {
-	// 	id: 0,
-	// 	name: "Black",
-	// 	colors: {
-	// 		center: "#363738",
-	// 		edge: "#0e0f0f",
-	// 		pattern: "#6c6868",
-	// 		text: "#8c8f91",
-	// 	},
-	// },
+	{
+		id: 0,
+		name: "Black",
+		colors: {
+			center: "#363738",
+			edge: "#0e0f0f",
+			pattern: "#6c6868",
+			text: "#8c8f91",
+		},
+	},
 	{
 		id: 1,
 		name: "Electric Purple",
