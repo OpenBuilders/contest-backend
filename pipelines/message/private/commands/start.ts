299abb2af21ac24df3502fd50aaf8370c354c63a
Erfan Mola <hi@erfanxd.ir>
2025-10-15 20:11:54 +0330

    fix: Remove my contests button

--- pipelines/message/private/commands/start.ts
+++ pipelines/message/private/commands/start.ts
@@ -8,24 +8,24 @@ export const handlerPrivateCommandStart: BotPipeline<
 	DBSchema
 > = async (message) => {
 	if (message.text?.startsWith("/start")) {
-		await sendMessage({
-			chat_id: message.chat.id,
-			text: t("en", "general.start.text", {
-				title: t("en", "general.name"),
-			}),
-			reply_parameters: {
-				message_id: message.message_id,
-				allow_sending_without_reply: true,
-			},
-			reply_markup: {
-				is_persistent: true,
-				resize_keyboard: true,
-				keyboard: [
-					[{ text: t("en", "general.menu.my") }],
-					// [{ text: t("en", "general.menu.create") }],
-				],
-			},
-		});
+		// await sendMessage({
+		// 	chat_id: message.chat.id,
+		// 	text: t("en", "general.start.text", {
+		// 		title: t("en", "general.name"),
+		// 	}),
+		// 	reply_parameters: {
+		// 		message_id: message.message_id,
+		// 		allow_sending_without_reply: true,
+		// 	},
+		// 	reply_markup: {
+		// 		is_persistent: true,
+		// 		resize_keyboard: true,
+		// 		keyboard: [
+		// 			[{ text: t("en", "general.menu.my") }],
+		// 			// [{ text: t("en", "general.menu.create") }],
+		// 		],
+		// 	},
+		// });
 
 		setState(message.chat.id, "private", {});
 	}
